ab1b21198044b44efb8fe2d05929b033a080d454
ProgramminCat <72707293+ProgramminCat@users.noreply.github.com>
2026-05-09 14:48:10 -0400

    user creation & authorization with database

--- server/main.go
+++ server/main.go
@@ -6,17 +6,41 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	_ "modernc.org/sqlite" // pure go because c is annoying
+	_ "modernc.org/sqlite" // pure go sqlite library because c is annoying
 )
+
+func BasicAuthWithDB(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		username, password, ok := c.Request.BasicAuth()
+		if !ok {
+			c.Header("WWW-Authenticate", `Basic realm="Restricted"`)
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		var user_id int
+		err := db.QueryRow(`SELECT user_id FROM users WHERE username = ? AND password = ?`, username, password,).Scan(&user_id)
+		if err != nil {
+			c.Header("WWW-Authenticate", `Basic realm="Restricted"`)
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		c.Set(gin.AuthUserKey, username)
+		c.Next()
+	}
+}
 
 func main() {
 
+	// Opens database, creates new if not exists
 	db, opendberr := sql.Open("sqlite", "pacedatabase.db")
 	if opendberr != nil {
 		log.Fatal(opendberr)
 	}
 	defer db.Close()
 
+	// Creates the users table if it doesnt exist
 	_, userstableerr := db.Exec(`
 	CREATE TABLE IF NOT EXISTS users (
 		user_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -31,16 +55,42 @@ func main() {
 
 	router := gin.Default()
 
-	basicauthrouter := router.Group("/", gin.BasicAuth(gin.Accounts{
-		"hardcodeduser": "password",
-		"anotherhardcodeduser": "password",
-	}))
+	basicauthrouter := router.Group("/", BasicAuthWithDB(db))
 
 	router.LoadHTMLGlob("templates/*")
 
-	basicauthrouter.GET("/", func(c *gin.Context) {
+	basicauthrouter.GET("/dashboard", func(c *gin.Context) {
 		user := c.MustGet(gin.AuthUserKey).(string) // gets the user from the basicauthrouter middleware
 		c.HTML(http.StatusOK, "index.html", gin.H{"user": user,})
+	})
+	router.GET("/", func(c *gin.Context) {
+		var usercount int
+		err := db.QueryRowContext(c, "SELECT COUNT(*) FROM users").Scan(&usercount)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if usercount == 0 {	
+			c.HTML(http.StatusOK, "setupfirstuser.html", nil)
+		} else {
+			c.Redirect(http.StatusFound, "/dashboard")
+		}
+	})
+	router.POST("/createfirstuser", func(c *gin.Context) {
+		var usercount int
+		err := db.QueryRowContext(c, "SELECT COUNT(*) FROM users").Scan(&usercount)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if usercount == 0 {
+			username := string(c.PostForm("username"))
+			password := string(c.PostForm("password"))
+
+			db.Exec(`INSERT INTO users (username, password) VALUES (?, ?)`, username, password)
+
+			c.JSON(200, gin.H{"success": true})
+		} else {
+			c.JSON(400, gin.H{"success": false, "message": "First_User_Already_Created"})
+		}
 	})
 	router.Run()
 }
